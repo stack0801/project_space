ae5a65e0132b756b312b513ce4c5cf4d7a100079
stack0801 <stack0801@gmail.com>
2024-07-15 21:40:06 +0900

    STYLE : 주석추가

--- main/main.js
+++ main/main.js
@@ -1,32 +1,17 @@
-const express = require('express');
-const { Worker } = require('worker_threads');
-const WebSocket = require('ws');
+const express = require('express');           // express 모듈
+const WebSocket = require('ws');              // 웹 소켓 모듈
+const { Worker } = require('worker_threads'); // 멀리스레드를 위한 worker_threads 라이브러리
 
+// http 서버 설정
 const app = express();
 const port = 3000;
 
-function runWorker(data) {
-  return new Promise((resolve, reject) => {
-      const worker = new Worker('./worker.js', { workerData: data });
-      worker.on('message', resolve);
-      worker.on('error', reject);
-      worker.on('exit', (code) => {
-          if (code !== 0)
-              reject(new Error(`Worker stopped with exit code ${code}`));
-      });
-  });
-}
-
-simulationWorker.on('message', (data) => {
-    simulationData = data;
-});
-
 app.get('/', (req, res) => {
-    res.send('WebSocket server is running. Open the console to see updates.');
+  res.send('WebSocket server is running. Open the console to see updates.');
 });
 
 const server = app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
 
 // WebSocket 서버 설정
@@ -45,3 +30,15 @@ wss.on('connection', (ws) => {
         clearInterval(sendDataInterval);
     });
 });
+
+function runWorker(data) {
+  return new Promise((resolve, reject) => {
+      const worker = new Worker('./worker.js', { workerData: data });
+      worker.on('message', resolve);
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+          if (code !== 0)
+              reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+  });
+}
